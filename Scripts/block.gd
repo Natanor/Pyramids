5cba5338bcd3df0819b156d20b022cf0977508c5
Todd Parker <tparker4068@gmail.com>
2024-01-07 07:06:09 +0000

    Added block shapes Added block rotation Added tracking camera and player Fixed physics jittering Shrunk Playspace

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 var player: Node2D
 var level: Node2D
 var isHeld: bool
+var dropping = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -22,14 +23,24 @@ func _process(delta):
 	if isHeld:
 		position.x = player.position.x
 		position.y = player.position.y + 20
+		if Input.is_action_just_pressed("Rotate"):
+			rotation_degrees = rotation_degrees + 90
 		if Input.is_action_just_pressed("Drop"):
 			isHeld = false
 			freeze = false
-			level._standbyNewBlock()
+			
+			level.standbyNewBlock()
+			await get_tree().create_timer(1.0).timeout
+			dropping = false
 		
 	if position.y > level.getKillHeight() and not freeze: # this is very finiky without a collider instead, but it sorta works
-		($Sprite2D as Sprite2D).modulate = Color(0.662745, 0.662745, 0.662745, 1)
-		freeze = true
+		#($Sprite2D as Sprite2D).modulate = Color(0.662745, 0.662745, 0.662745, 1)
+		#freeze = true
 		
-		level.lives -= 1;
+		level.loseLife()
+		print_debug(str(level.lives))
+		queue_free()
 		
+	if linear_velocity.y <= 0.05 && !isHeld:
+		#print_debug("stopped")
+		set_physics_process(false)
